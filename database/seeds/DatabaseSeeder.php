360f15af47828f74ddb52a599a3a307018d7f087
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2020-07-21 17:52:39 +0545

    database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Question;
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(User::class, 10)->create()
+            ->each(function ($user) {
+                $user->questions()
+                    ->saveMany(
+                        factory(Question::class, rand(1, 5)))->make();
+
+            });
     }
 }
