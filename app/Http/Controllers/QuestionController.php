c8d9d2dad428e84394167ca860e65fc6b6a92269
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2020-07-22 15:10:22 +0545

    implement questions store with flash messeging

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\QuestionStoreRequest;
 use App\Question;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -27,7 +28,8 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+        return view('questions.create', compact('question'));
     }
 
     /**
@@ -36,9 +38,16 @@ class QuestionController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(QuestionStoreRequest $request)
     {
-        //
+        $request->user()->questions()->create(
+            [
+                'title' => $request->title,
+                'body' => $request->body,
+            ]
+        );
+
+        return redirect()->route('questions.index')->with('success', 'Your question has been submitted.');
     }
 
     /**
