0d9a2c1310043e83bf959731833bc64f15b4fdf0
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2020-07-22 21:46:47 +0545

    delete question implementation

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -37,6 +37,15 @@
                                         <div class="ml-auto">
                                             <a href="{{route('questions.edit', $question->id)}}"
                                                class="btn btn-sm btn-outline-info">Edit</a>
+                                            <form method="post" class="form-delete"
+                                                  action="{{route('questions.destroy', $question->id)}}">
+                                                @method('delete')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger"
+                                                        onclick="return confirm('Are you sure?')">Delete
+                                                </button>
+                                            </form>
+
                                         </div>
 
                                     </div>
