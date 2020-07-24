c5530d27d0b0141e2af95ea64f7225d3abf4f7d6
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2020-07-24 14:57:48 +0545

    edit answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -25,18 +25,42 @@
                         </div>
                         <div class="media-body">
                             {!! $answer->body !!}
-                            <div class="float-right">
-                                <span class="text-muted"> Answered {{$answer->created_date}}</span>
-                                <div class="media">
-                                    <a href="{{$answer->user->url}}" class="pr-2">
-                                        <img src="{{$answer->user->avatar}}">
-                                        {{--                                                <img src="https://www.gravatar.com/avatar/{{md5(strtolower(trim('raj@mail.com')))}}?s=32">--}}
-                                    </a>
-                                    <div class="media-body mt-1">
-                                        <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="ml-auto">
+                                        @can('update', $answer)
+                                            <a href="{{route('questions.answers.edit', [$question->id, $answer->id])}}"
+                                               class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endcan
+                                        @can('delete', $answer)
+                                            <form method="post" class="form-delete"
+                                                  action="{{route('questions.answers.destroy', [$question->id, $answer->id])}}">
+                                                @method('delete')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger"
+                                                        onclick="return confirm('Are you sure?')">Delete
+                                                </button>
+                                            </form>
+                                        @endcan
+
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                </div>
+                                <div class="col-4">
+                                    <span class="text-muted"> Answered {{$answer->created_date}}</span>
+                                    <div class="media">
+                                        <a href="{{$answer->user->url}}" class="pr-2">
+                                            <img src="{{$answer->user->avatar}}">
+                                            {{--                                                <img src="https://www.gravatar.com/avatar/{{md5(strtolower(trim('raj@mail.com')))}}?s=32">--}}
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                     <hr>
