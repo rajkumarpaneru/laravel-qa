4c78d9ae0088776d0c9345a5687b56ea19ae4af4
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2020-08-12 15:53:13 +0545

    refactor views

---
+++ resources/views/answers/_answer.blade.php
@@ -0,0 +1,37 @@
+<div class="media post">
+    @include('shared._vote', [
+    'model' => $answer
+    ])
+    <div class="media-body">
+        {!! $answer->body !!}
+        <div class="row">
+            <div class="col-4">
+                <div class="ml-auto">
+                    @can('update', $answer)
+                        <a href="{{route('questions.answers.edit', [$question->id, $answer->id])}}"
+                           class="btn btn-sm btn-outline-info">Edit</a>
+                    @endcan
+                    @can('delete', $answer)
+                        <form method="post" class="form-delete"
+                              action="{{route('questions.answers.destroy', [$question->id, $answer->id])}}">
+                            @method('delete')
+                            @csrf
+                            <button type="submit" class="btn btn-sm btn-outline-danger"
+                                    onclick="return confirm('Are you sure?')">Delete
+                            </button>
+                        </form>
+                    @endcan
+
+                </div>
+            </div>
+            <div class="col-4">
+            </div>
+            <div class="col-4">
+                @include('shared._author',[
+                                'model' => $answer,
+                                'label' => 'answered'
+                                          ])
+            </div>
+        </div>
+    </div>
+</div>
