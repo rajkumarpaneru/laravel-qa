cfef73d827a3cf974ebff5e0143ce8b3ec7c4748
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2020-08-12 12:01:12 +0545

    refactor repeated code

--- app/Answer.php
+++ app/Answer.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    use VotableTrait;
+
     protected $guarded = [];
 
     public function user()
@@ -49,18 +51,4 @@ class Answer extends Model
         return $this->id === $this->question->best_answer_id;
     }
 
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
-    }
 }
